aad8cd1ef3fdbb2f49f2b6e711bec428e549a1e0
Alex Bruno <alexbrunops@gmail.com>
2020-01-06 16:20:44 -0300

    Implementação da busca por filtro.
    - Implementação da busca de contatos por filtro;
    - Implementação de uma classe Specification para tratar a busca por criteria;
    - Refatoração do método de update;
    - Extensão da JpaSpecificationExecutor para o repositório;
    - implementação do método updateContact na classe Contact.

---
+++ src/main/java/com/wex/gateway/dev_evaluation_project/model/ContactSpecification.kt
@@ -0,0 +1,23 @@
+package com.wex.gateway.dev_evaluation_project.model
+
+import org.springframework.data.jpa.domain.Specification
+import javax.persistence.criteria.CriteriaBuilder
+import javax.persistence.criteria.CriteriaQuery
+import javax.persistence.criteria.Predicate
+import javax.persistence.criteria.Root
+
+// Necessário a implementação de uma classe Specification para tratar a busca por criteria
+class ContactSpecification(var criteria: Contact?) : Specification<Contact> {
+
+    override fun toPredicate(root: Root<Contact>, query: CriteriaQuery<*>, builder: CriteriaBuilder): Predicate? {
+        query.orderBy()
+        return builder.or(
+                builder.like(root.get<String>("name"), "%" + this.criteria?.name + "%"),
+                builder.like(root.get<String>("nickname"), "%" + this.criteria?.nickname + "%"),
+                builder.like(root.get<String>("email"), "%" + this.criteria?.email + "%"),
+                builder.like(root.get<String>("address"), "%" + this.criteria?.address + "%"),
+                builder.equal(root.get<Contact>("id"), this.criteria?.id)
+        )
+    }
+
+}
